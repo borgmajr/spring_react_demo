d1d82c0101b368bd8ea2d9e5f6f0b3acf3cbf11e
jon borgman <jon.borgman@lnssi.com>
2020-09-26 16:21:29 -0400

    first rest point

---
+++ src/main/java/com/borgman/starterapp/web/system/SystemTimeController.java
@@ -0,0 +1,16 @@
+package com.borgman.starterapp.web.system;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
+
+@RestController
+public class SystemTimeController {
+
+	@GetMapping("/api/system/time")
+	public String time() {
+		return "Hello, the time at the server is now " + new Date() + "\n";
+	}
+
+}
